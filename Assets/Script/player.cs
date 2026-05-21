6352ff26a76bf1fb4147a4c01b40ce57a7676014
herobest01 <a01025639154@gmail.com>
2026-05-22 03:18:56 +0900

    computeranimation_pj

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class player : MonoBehaviour
 {
@@ -9,11 +10,19 @@ public class player : MonoBehaviour
     public float jumpDuration = 1f;
 
     private SpriteRenderer spriteRenderer;
+
     private bool isJumping = false;
     private float jumpTimer = 0f;
+
     private Vector3 startPosition;
+
     private bool isMovingRight = false;
+
     private bool isWatchRight = true;
+
+    private bool doAttack = false;
+    //private float attackDuration = 1f;
+    //private float attackTimer = 0f;
 
     [Header("Animation Controller")]
     //player ready
@@ -25,10 +34,13 @@ public class player : MonoBehaviour
     public RuntimeAnimatorController player_run_left_controller;
     public RuntimeAnimatorController player_run_right_controller;
     //player attack
-    //public RuntimeAnimatorController player_attack_right_controller;
-    //public RuntimeAnimatorController player_attack_left_controller;
+    public RuntimeAnimatorController player_attack_right_controller;
+    public RuntimeAnimatorController player_attack_left_controller;
 
     private Animator animator;
+
+    [Header("Animation Duration")]
+    private float player_attack_right_duration = 0.4f;
 
     void Start()
     {
@@ -38,6 +50,8 @@ public class player : MonoBehaviour
     void Update()
     {
         Vector2 moveDirection = Vector2.zero;
+
+        if(doAttack == true) return;
 
         // 캐릭터 이동 & animator
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -54,16 +68,10 @@ public class player : MonoBehaviour
             RunAnimator();
         }
 
+        //player가 바라보고있는 방향 판별
         if (moveDirection.x == 0)
         {
-            if(isWatchRight == true)
-            {
-                animator.runtimeAnimatorController = player_idle_right_controller;
-            }
-            else if (isWatchRight == false)
-            {
-                animator.runtimeAnimatorController = player_idle_left_controller;
-            }
+            WatchAnimation();
         }
 
         moveDirection = moveDirection.normalized;
@@ -78,6 +86,28 @@ public class player : MonoBehaviour
         {
             UpdateJump();
         }
+
+        //공격기능
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            doAttack = true;
+            StartCoroutine(Attack());
+        }
+    }
+
+    void WatchAnimation()
+    {
+        if(isWatchRight == true)
+        {
+            animator.runtimeAnimatorController = player_idle_right_controller;
+            Debug.Log("watchanimation/right");
+        }
+        else if(isWatchRight == false)
+        {
+            animator.runtimeAnimatorController = player_idle_left_controller;
+            Debug.Log("watchanimation/left");
+        }
+        else return;
     }
 
     //달리기 애니메이터
@@ -117,5 +147,52 @@ public class player : MonoBehaviour
             float height = Mathf.Sin(progress * Mathf.PI) * jumpHeight;
             transform.position = new Vector3(transform.position.x, startPosition.y + height, transform.position.z);
         }
+    }
+
+    //공격 관련 함수
+    IEnumerator Attack()
+    {   
+        /*
+        if(player_attack_right_controller == null || player_attack_left_controller == null)
+        {
+            return;
+        }
+        */
+
+        if(doAttack == true){
+            if(isWatchRight == true)
+            {
+                animator.runtimeAnimatorController = player_attack_right_controller;
+
+                yield return null;
+
+                animator.Play(0);
+                //Debug.Log("animator.Play시작");
+
+                yield return new WaitForSeconds(player_attack_right_duration);
+
+                animator.runtimeAnimatorController = player_idle_right_controller;
+
+                yield return null;
+                
+                animator.Play(0);
+                //Debug.Log("animator.idle");
+
+            }
+            else if(isWatchRight == false)
+            {
+                animator.runtimeAnimatorController = player_attack_left_controller;
+                EndAttack();
+                animator.runtimeAnimatorController = player_idle_left_controller;
+            }
+
+            doAttack = false;
+        }
+    }
+
+    void EndAttack()
+    {
+        doAttack = false;
+        return;
     }
 }
